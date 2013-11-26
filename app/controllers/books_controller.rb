52e5f77b80ed8873d74f1909fc83138d7313edbf
fernando <fergarza90@gmail.com>
2013-11-26 04:39:00 +0000

    probando nuevamente en heroku

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,43 +1,26 @@
 class BooksController < ApplicationController
   def book
     @book= Book.all
+    @author=Author.all
    #render :json=> @autor
   end
   def index
     
   end
-  
-   # PUT /cars/1
-  # PUT /cars/1.json
-  def update
-    @book = Book.find(params[:id])
+ 
+#  def update
+#    @book = Book.find(params[:id])
 
-    respond_to do |format|
-      if @book.update_attributes(params[:book])
-        format.html { redirect_to @book, notice: 'Libro Actuaalizado.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-  
-   # PUT /cars/1
-  # PUT /cars/1.json
-  def update
-    @book = Book.find(params[:id])
-
-    respond_to do |format|
-      if @Book.update_attributes(params[:book])
-        format.html { redirect_to @book, notice: 'Libro Actuaalizado.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+#    respond_to do |format|
+ #     if @Book.update_attributes(params[:book])
+  #      format.html { redirect_to @book, notice: 'Libro Actuaalizado.' }
+   #     format.json { head :no_content }
+   #   else
+    #    format.html { render action: "edit" }
+     #   format.json { render json: @book.errors, status: :unprocessable_entity }
+      #end
+    #end
+  #end
   
   def create
     @book = Book.new(book_params)
@@ -48,26 +31,26 @@ class BooksController < ApplicationController
     render :action =>'new'
   end
 end
-   def show
-    @book = Book.find(params[:id])
+#   def show
+#    @book = Book.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @car }
-    end
-  end
+    #respond_to do |format|
+     # format.html # show.html.erb
+     # format.json { render json: @car }
+    #end
+  #end
   
    # DELETE /cars/1
   # DELETE /cars/1.json
-  def destroy
-    @book = Book.find(params[:id])
-    @Book.destroy
+#  def destroy
+ #   @book = Book.find(params[:id])
+  #  @Book.destroy
 
-    respond_to do |format|
-      format.html { redirect_to books_url }
-      format.json { head :no_content }
-    end
-  end
+ #   respond_to do |format|
+  #    format.html { redirect_to books_url }
+   #   format.json { head :no_content }
+    #end
+#  end
   
 
   
